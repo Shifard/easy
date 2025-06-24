fe5baa644689dd5a04a3cb1a1f7a1b2af9ac46a9
marcoxmediran <marcoxmediran@gmail.com>
2025-06-24 14:57:53 +0800

    chore: add blog model, migration, and blog seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Blog;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -33,6 +34,31 @@ class DatabaseSeeder extends Seeder
             'username' => 'testuser',
             'email' => 'test@email.com',
             'password' => bcrypt('12345678'),
+        ]);
+
+        // Create blogs
+        Blog::create([
+            'user_id' => 1,
+            'title' => 'Admin Blog 1',
+            'description' => 'Admin Description 1',
+            'slug' => 'admin-blog-1',
+            'content' => 'Admin Content 1',
+        ]);
+
+        Blog::create([
+            'user_id' => 1,
+            'title' => 'Admin Blog 2',
+            'description' => 'Admin Description 2',
+            'slug' => 'admin-blog-2',
+            'content' => 'Admin Content 2',
+        ]);
+
+        Blog::create([
+            'user_id' => 2,
+            'title' => 'Marcox Blog 1',
+            'description' => 'Marcox Description 1',
+            'slug' => 'marcox-blog-1',
+            'content' => 'Marcox Content 1',
         ]);
     }
 }
